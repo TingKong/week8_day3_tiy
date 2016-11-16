b9817d0f88c525db5934c38228796dc01ce7819e
TingKong <kongsau@gmail.com>
2016-11-16 15:04:32 -0500

    uploading files

---
+++ weather/weather/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using weather.Models;
+
+namespace weather.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View("Weather2");
+        }
+
+        public JsonResult GetWeather()
+        {
+            weather_stuff myWeather = new weather_stuff();
+            return Json(myWeather.GetCurrentWeather(), JsonRequestBehavior.AllowGet);
+
+        }
+    }
+}
---
+++ weather/weather/Models/weather_stuff.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Script.Serialization;
+
+namespace weather.Models
+{
+    public class weather_stuff
+    {
+        public object GetCurrentWeather()
+        {
+            string url = "http://api.openweathermap.org/data/2.5/weather?q=Detroit&APPID=f034c681a6edc9235b67986bf6d680cc&units=imperial";
+            var client = new WebClient();
+            var content = client.DownloadString(url);
+            var serializer = new JavaScriptSerializer();
+            var jsonContent = serializer.Deserialize<object>(content);
+
+            return jsonContent;
+
+        }
+    }
+}
